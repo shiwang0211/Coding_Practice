b927e6f8fdf3991c948e5abc864217021dcc3f31
shiwang <shiwang0211@gmail.com>
2018-10-02 23:42:29 -0700

    update

--- Exercise for Interview.sql
+++ Exercise for Interview.sql
@@ -1,3 +1,4 @@
+-- https://www.w3schools.com/sql/trysql.asp?filename=trysql_op_in
 -- three table join
 select a.id, b.id, c.id from
 	TableA as a 
@@ -8,3 +9,24 @@ select a.id, b.id, c.id from
 select a.id, b.id from a
 	inner join (select * from c) as b
 	on a.id = b.id
+
+--- everything
+SELECT  * FROM Customers as a
+INNER JOIN (select * from Orders) as b
+on a.CustomerID = b.CustomerID
+where Country = 'USA'
+group by City
+having count(EmployeeID) > 2 
+
+-- left join = full join + is not null check
+SELECT DISTINCT * FROM Customers as a
+LEFT JOIN (select * from Orders) as b
+on a.CustomerID = b.CustomerID
+where b.OrderID is not NULL
+
+-- IN, between
+SELECT DISTINCT * FROM Customers as a
+INNER JOIN (select * from Orders) as b
+on a.CustomerID = b.CustomerID
+and a.Country in ('USA','Finland')
+and b.EmployeeID BETWEEN 1 AND 5
